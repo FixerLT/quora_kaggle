c8f068c6fe294c44d542dc88dabb2fe0092682ce
Mark Barho <zazazasaza@mail.ru>
2018-04-13 17:14:05 +0300

    initial code

---
+++ main.py
@@ -0,0 +1,79 @@
+import nltk.stem
+import csv
+import scipy as sp
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy
+
+def dist_norm(v1, v2):
+    v1_normalized = v1/sp.linalg.norm(v1.toarray());
+    v2_normalized = v2/sp.linalg.norm(v2.toarray());
+    delta = v1_normalized - v2_normalized;
+    return sp.linalg.norm(delta.toarray());
+
+raw_data = []
+with open("train.csv") as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
+    for row in reader: # each row is a list
+        raw_data.append(row)
+#raw_data = raw_data[:int(len(raw_data)/10)];
+raw_words = []
+raw_pairs = []
+answers = []
+for i in range(1, len(raw_data)):
+    raw_words.append(raw_data[i][3])
+    raw_words.append(raw_data[i][4])
+    raw_pairs.append(raw_data[i][3:5])
+    answers.append(raw_data[i][5])
+
+english_stemmer = nltk.stem.SnowballStemmer("english")
+class StemmedCountVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        analyzer = super(StemmedCountVectorizer, self).build_analyzer()
+        return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
+
+
+vectorizer = StemmedCountVectorizer(min_df=1, stop_words="english")
+print("test")
+vectorizer.fit(raw_words)
+print("test")
+
+def vect(arr):
+    new_arr = [arr]
+    return vectorizer.transform(new_arr);
+
+pairs = []
+for row in raw_pairs:
+    new_row = []
+    for sentence in row:
+        new_row.append(vect(sentence))
+    pairs.append(new_row)
+#vectorizer.transform(raw_pairs)
+print(len(pairs))
+
+good_pos = []
+good_neg = []
+for i in range(0, len(pairs)):
+    ans = dist_norm(pairs[i][0], pairs[i][1]);
+    pos = int(ans*1000);
+    if answers[i] == '1':
+        good_pos.append(ans);
+    else:
+        good_neg.append(ans);
+print(sum(good_pos) / len(good_pos));
+print(sum(good_neg) / len(good_neg));
+lim = (sum(good_pos) / len(good_pos) + sum(good_neg) / len(good_neg)) / 2;
+test_answers = [];
+print("we are fine over here");
+with open("test.csv") as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
+    for row in reader: # each row is a list
+        new_dist = dist_norm(vect(row[1]), vect(row[2]));
+        new_ans = '0';
+        if(new_dist < lim):
+            new_ans = '1';
+        test_answers.append(new_ans);
+with open('answers.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['test_id','is_duplicate']);
+    for i in range(1, len(test_answers)):
+        writer.writerow([i-1,int(test_answers[i])]);
